26ab00f91448bc6bb477161cb33abfac5bbff2d5
Jan Václavík <jan.vaclavik@profinit.eu>
2021-01-12 16:28:45 +0100

    Removed getAwaiters

--- NasaApi/Program.cs
+++ NasaApi/Program.cs
@@ -2,6 +2,7 @@
 using NasaApi.ResultBuilders;
 using NasaApi.Services;
 using System;
+using System.Threading.Tasks;
 
 namespace NasaApi
 {
@@ -22,14 +23,15 @@ namespace NasaApi
 
             // Print first five Mars surface images hrefs
             var url = "https://images-api.nasa.gov/search?media_type=image&year_start=2018&year_end=2018&keywords=mars";
-            program.AnalyzeImages(url).GetAwaiter().GetResult();
+            program.AnalyzeImages(url);
+
 
             Console.WriteLine("---------------------------------");
             Console.WriteLine("2st task:");
 
             // Print info from media library
             url = "https://images-api.nasa.gov/search?media_type=video&year_start=2018&year_end=2018&keywords=mars";
-            program.AnalyzeVideoLibrary(url).GetAwaiter().GetResult();
+            program.AnalyzeVideoLibrary(url);
 
             Console.WriteLine("Application finished");
             Console.ReadLine();
